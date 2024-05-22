42737a3812d99bb4e6efe7e3746f571fd0242f25
sanjitsg <sanjeet25.sg@gmail.com>
2024-05-23 01:59:45 +0530

    middleware

--- server-side-rendering/middleware.js
+++ server-side-rendering/middleware.js
@@ -3,8 +3,23 @@ import express from "express";
 const app = express();
 const PORT = 5000;
 
+const middleware = (req, res, next) => {
+  console.log("Middleware-one");
+  next();
+};
+
+app.use(middleware);
+
 app.get("/", (req, res) => {
-  res.status(200).send("<h1>Middleware..</h1>");
+  res.status(200).send("<h1>Home Page..</h1>");
+});
+
+app.get("/about", (req, res) => {
+  res.status(200).send("<h1>About Page..</h1>");
+});
+
+app.get("/contact", (req, res) => {
+  res.status(200).send("<h1>Contact Page..</h1>");
 });
 
 app.listen(PORT, () => console.log(`Server is running on: http://localhost:${PORT}`));
